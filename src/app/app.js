a7e838b7a05b947c5375a4ca7fe941123794c69a
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-02-26 11:39:05 -0300

    añadir options a APP

--- src/app/app.js
+++ src/app/app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(function(req, res, next) { // VER BIEN LA EXPLICACIÓN DE ESTOOOOOOOOOOOOOOOOOO
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
